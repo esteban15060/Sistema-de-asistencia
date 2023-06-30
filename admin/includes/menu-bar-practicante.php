0fa22ba55068fb9df96597e8f154ab80c656729d
Christian Leon <jesusleonangulo@hotmail.com>
2023-06-30 18:29:58 -0500

    NavBar
    - Se diseño el NavBar

--- admin/includes/menu-bar-practicante.php
+++ admin/includes/menu-bar-practicante.php
@@ -1,38 +1,16 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary" style="margin-left: 150px;">
+<nav class="navbar navbar-expand-lg bg-nav" style="margin-left: 150px;">
    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-               <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-               <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-               <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-               Dropdown
-               </a>
-               <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li>
-                     <hr class="dropdown-divider">
-                  </li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-               </ul>
-            </li>
-            <li class="nav-item">
-               <a class="nav-link disabled">Disabled</a>
-            </li>
-         </ul>
-         <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-         </form>
+      <div class="input-group mb-3 diseño-escribir-aqui">
+        <input type="text" class="form-control" placeholder="Escribe aqui...">
+        <span class="ps-5"></span>
+      </div>
+      <div class="d-flex posicion">
+        <h6 class="pe-3 letraNavBar posicion-fecha">22 Junio</h6>
+        <img src="../../iconos/notificacion.webp" alt="notificacion" height="40" width="40">
+      </div>
+      <div class="circle-container d-flex me-5">
+        <img class="rounded-circle" src="../images/fondo-prueba.png" alt="perfil" height="40" width="40">
+        <h6 class="ps-3 letraNavBar posicion-fecha">María Hernandez</h6>
       </div>
    </div>
 </nav>
