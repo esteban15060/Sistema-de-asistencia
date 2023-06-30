abca6882dcca3153b237a944bb96956cf92617c3
Christian Leon <jesusleonangulo@hotmail.com>
2023-06-30 15:58:16 -0500

    Siderbar
    - Se realizo el diseño.
    - Se realizo animación al presionar botón.

--- admin/home-practicante.php
+++ admin/home-practicante.php
@@ -1,9 +1,11 @@
-<?php include 'includes/menu-bar-practicante.php'; ?>
-<?php include 'includes/header.php'; ?>
+<?php include 'includes/header-practicante.php'; ?>
 
-<body class="bg-success">
-    <div>
- hola
+<body class="bg-white">
+    <?php include 'includes/menu-bar-practicante.php'; ?>
+    <div style="margin-left: 280px; margin-top: 30px; padding: 0 20px;">
+        <div>
+            <h1 class="text-center">Hola</h1>
+        </div>
     </div>
 </body>
 
