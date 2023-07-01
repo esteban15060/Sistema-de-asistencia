a7176fa7a2de542b87056e758bc1239798275aea
Christian Leon <jesusleonangulo@hotmail.com>
2023-06-30 21:19:31 -0500

    Creando estructura para perfil

--- admin/home-practicante.php
+++ admin/home-practicante.php
@@ -4,8 +4,25 @@
     <?php include 'includes/menu-bar-practicante.php'; ?>
     <div style="margin-left: 280px; margin-top: 30px; padding: 0 20px;">
         <div>
-            <h1 class="text-center">Hola</h1>
+            <img src="../images/cielo.webp">
         </div>
+        <div class="estilo-card">
+            <h1>Título de la tarjeta</h1>
+            <p>Contenido de la tarjeta.</p>
+        </div>
+        <div class="estilo-card">
+            <h1>Título de la tarjeta</h1>
+            <p>Contenido de la tarjeta.</p>
+        </div>
+        <div class="estilo-card">
+            <h1>Título de la tarjeta</h1>
+            <p>Contenido de la tarjeta.</p>
+        </div>
+        <div class="estilo-card">
+            <h1>Título de la tarjeta</h1>
+            <p>Contenido de la tarjeta.</p>
+        </div>
+    </div>
     </div>
 </body>
 
