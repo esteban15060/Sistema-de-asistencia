703744a94db892378282dc1de524328799bcf6cb
Allison Andrea Rodriguez Sanchez <allisonandrea712@gmail.com>
2023-07-01 11:18:30 -0500

    seccion perfil lista (diseño)
    solo es diseño

--- admin/home-practicante.php
+++ admin/home-practicante.php
@@ -3,25 +3,7 @@
 <body class="bg-white">
     <?php include 'includes/menu-bar-practicante.php'; ?>
     <div style="margin-left: 280px; margin-top: 30px; padding: 0 20px;">
-        <div>
-            <img src="../images/cielo.webp">
-        </div>
-        <div class="estilo-card">
-            <h1>Título de la tarjeta</h1>
-            <p>Contenido de la tarjeta.</p>
-        </div>
-        <div class="estilo-card">
-            <h1>Título de la tarjeta</h1>
-            <p>Contenido de la tarjeta.</p>
-        </div>
-        <div class="estilo-card">
-            <h1>Título de la tarjeta</h1>
-            <p>Contenido de la tarjeta.</p>
-        </div>
-        <div class="estilo-card">
-            <h1>Título de la tarjeta</h1>
-            <p>Contenido de la tarjeta.</p>
-        </div>
+        
     </div>
     </div>
 </body>
--- admin/includes/menu-bar-practicante.php
+++ admin/includes/menu-bar-practicante.php
@@ -6,7 +6,7 @@
       </div>
       <div class="d-flex posicion">
         <h6 class="pe-3 letraNavBar posicion-fecha">22 Junio</h6>
-        <img src="../../iconos/notificacion.webp" alt="notificacion" height="40" width="40">
+        <img src="../img/notificacion.webp" alt="notificacion" height="40" width="40">
       </div>
       <div class="circle-container d-flex me-5">
         <img class="rounded-circle" src="../images/fondo-prueba.png" alt="perfil" height="40" width="40">
@@ -20,28 +20,28 @@
    </div>
    <div class="nav-items">
       <a class="enlaces" href="home-practicante.php" onclick="toggleButtonColor(event, 'button1')" id="button1" >
-         <img class="icono-centrar" src="../../iconos/icono-home.webp" height="40" width="40">
+         <img class="icono-centrar" src="../img/icono-home.webp" height="40" width="40">
          <h6 class="text-center text-light letraNavBar">INICIO</h6>
       </a>
       <br>
-      <a class="enlaces" href="home-practicante.php" onclick="toggleButtonColor(event, 'button2')" id="button2">
-         <img class="icono-centrar" src="../../iconos/icono-perfil.webp" height="40" width="40">
+      <a class="enlaces" href="perfil-practicante.php" onclick="toggleButtonColor(event, 'button2')" id="button2">
+         <img class="icono-centrar" src="../img/icono-perfil.webp" height="40" width="40">
          <h6 class="text-center text-light letraNavBar">MI PERFIL</h6>
       </a>
       <br>
       <a class="enlaces" href="home-practicante.php" onclick="toggleButtonColor(event, 'button3')" id="button3">
-         <img class="icono-centrar" src="../../iconos/icono-estadistica.webp" height="40" width="40">
+         <img class="icono-centrar" src="../img/icono-estadistica.webp" height="40" width="40">
          <h6 class="text-center text-light letraNavBar" style="width: 120px; margin-left: 15px;">ESTADISTICAS Y LOGROS</h6>
       </a>
       <br>
       <a class="enlaces" href="home-practicante.php" onclick="toggleButtonColor(event, 'button4')" id="button4">
-         <img class="icono-centrar" src="../../iconos/icono-calendario.webp" height="40" width="40">
+         <img class="icono-centrar" src="../img/icono-calendario.webp" height="40" width="40">
          <h6 class="text-center text-light letraNavBar" style="width: 110px; margin-left: 20px;">CALENDARIO O AGENDA</h6>
       </a>
    </div>
    <div class="mb-3">
       <a class="enlaces" href="#">
-         <img class="icono-centrar" src="../../iconos/icono-salir.webp" height="40" width="40">
+         <img class="icono-centrar" src="../img/icono-salir.webp" height="40" width="40">
          <h6 class="text-center text-light letraNavBar">SALIR</h6>
       </a>
    </div>
