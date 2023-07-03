8cc3ed76ebe849e3f55ff4142e0171ea4375283b
Allison Andrea Rodriguez Sanchez <allisonandrea712@gmail.com>
2023-07-03 14:00:18 -0500

    AÑADI CARD DE PERFIL PRACTICANTE

--- admin/perfil-practicante.php
+++ admin/perfil-practicante.php
@@ -2,26 +2,27 @@
 
 <body class="bg-white">
     <?php include 'includes/menu-bar-practicante.php'; ?>
-    <div style="margin-left: 200px; margin-top: 30px; container-fluid">
-        <div class="estilo-card-perfil">
-          <div class="d-flex">
+    <div style="margin-left: 200px; margin-top: 30px;" class="container-fluid" id="grid-perfil">
+        <div class="d-flex estilo-card-perfil">
             
-            <div>
-                <h2 class="letraNavBar ps-5 pt-2 colorletraperfil">Bienvenida, Maria</h2>
-                <small class="letraNavBar ps-5 pt-1 colorletraperfil fw-bold text-wraper">Aquí encontrarás información necesaria sobre tus estadídticas y desempeño.</small>
-                <div class="d-flex pt-3">
-                    <h6 class="letraNavBar ps-5 colorletraperfil fw-normal" style="margin-left: 30px;">Comunicaciones</h6>
+            <div class="tarjeta-perfil" id="perfil">
+                <div>
+                    <h2 class="letraNavBar colorletraperfil">Bienvenida, María</h2>
+                    <p class="letraNavBar colorletraperfil  text-wraper">Aquí encontrarás información necesaria sobre tus estadísticas y desempeño.</p>
+                </div>
+                <div class="d-flex perfil-presentacion">
+                    <h6 class="letraNavBar  colorletraperfil fw-normal" style="margin-left: 30px;">Comunicaciones</h6>
                     <h6 class="letraNavBar colorletraperfil fw-normal" style="margin-left: 70px;">Pre Profesional</h6>
                 </div>
-                <h6 class="letraNavBar ps-5 colorletraperfil fw-normal" style="margin-left: 30px;">Digimedia</h6>
-                <div class="d-flex ps-5">
-                    <h6 class="letraNavBar ps-2 diseñofechaingresoysalidad fw-normal">Ingreso:</h6>
+                <h6 class="letraNavBar perfil-presentacion2 colorletraperfil fw-normal" style="margin-left: 30px;">Digimedia</h6>
+                <div class="d-flex perfil-contrato">
+                    <h6 class="letraNavBar diseñoFechaIngreso">Ingreso:</h6>
                      <div class="ps-1 d-flex contenedor-fecha ms-2">
                          <h6 class="letraNavBar posicion-border3 diseñofecha text-center">07</h6>
                          <h6 class="letraNavBar posicion-border1 diseñofecha text-center" style="margin-left: 10px;">05</h6>
                          <h6 class="letraNavBar posicion-border2 diseñofecha text-center" style="margin-left: 10px;">23</h6>
                      </div>
-                    <h6 class="letraNavBar ps-2 diseñofechaingresoysalidad fw-normal" style="margin-left: 50px;">Salida:</h6>
+                    <h6 class="letraNavBar diseñoFechaSalida">Salida:</h6>
                     <div class="ps-1 d-flex contenedor-fecha ms-2">
                          <h6 class="letraNavBar posicion-border3  diseñofecha text-center">09</h6>
                          <h6 class="letraNavBar posicion-border1  diseñofecha text-center" style="margin-left: 10px;">08</h6>
@@ -29,15 +30,32 @@
                      </div>
                 </div>
             </div>
-            <div  style="margin-left: 60px; margin-top: 20px;">
+            <div  class="perfil-yuntas">
                 <img src="../img/yuntas.webp">
             </div>
-          </div>
+        </div>
+        <div class="card perfil-estadistica-desempeño" id="estadistica-desempeño">
+            <h1>Título de la tarjeta</h1>
+            <p>Contenido de la tarjeta.</p>
+        </div>
+        <div class="card perfil-estado-perfil" id="estado">
+            <h1>Título de la tarjeta</h1>
+            <p>Contenido de la tarjeta.</p>
+        </div>
+        <div class="card perfil-logros" id="logros">
+            <h1>Título de la tarjeta</h1>
+            <p>Contenido de la tarjeta.</p>
+        </div>
+        
         </div>
        
     </div>
     
 </body>
+
+
+
+
 
 
 
