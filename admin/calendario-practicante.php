67ffc093b93afdb780f19cea1db4f94b80a9fa59
Allison Andrea Rodriguez Sanchez <allisonandrea712@gmail.com>
2023-07-05 13:02:02 -0500

    corregir errores

--- admin/calendario-practicante.php
+++ admin/calendario-practicante.php
@@ -2,8 +2,8 @@
 
 <body class="bg-white">
     <?php include 'includes/menu-bar-practicante.php'; ?>
-    <div style="margin-left: 200px; margin-top: 30px; container-fluid">
-        <div class="buttons">
+    <div style="margin-left: 100px; margin-top: 30px;" class="container-fluid">
+        <div class="buttons" style="margin-left: 100px;">
             <button class="active">MIS TAREAS</button>
             <button>EVENTOS</button>
             <button>GENERAL</button>
@@ -11,10 +11,7 @@
         <div id="calendar" class="container pb-5"></div>
 
         <script src="../dist/js/calendar.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
-            crossorigin="anonymous"></script>
-
+        
     </div>
 
 </body>
