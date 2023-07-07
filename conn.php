322c5b7864316206901129d39766fb6b23bb153c
72092900 <72092900@certus.edu.pe>
2023-07-07 12:12:34 -0500

    local

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('127.0.0.1:3310', 'root', '', 'asistencia');
+	$conn = new mysqli('localhost', 'root', '', 'asistencia');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
