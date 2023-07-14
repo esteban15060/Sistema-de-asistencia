4a272fe1616ba922beca2aa4d94512d81fc6dc5b
Allison Andrea Rodriguez Sanchez <allisonandrea712@gmail.com>
2023-07-14 13:05:23 -0500

    Fondo aleatorio login de admin

--- admin/index.php
+++ admin/index.php
@@ -6,13 +6,13 @@ if (isset($_SESSION['admin'])) {
 ?>
 <?php include 'includes/header.php'; ?>
 
-<body class="hold-transition login-page fondoimagen-admin">
+<body id="fondo" class="hold-transition login-page fondoimagen-admin">
 	<div class="login-box">
 		<div class="login-logo titu">
 			<h3>PANEL DE CONTROL</h3>
 		</div>
 
-		<div class="login-admin">
+		<div class="login-admin" id="fondo-imagen">
 
 			<form action="login.php" method="POST">
 				<div class="form-group has-feedback">
@@ -44,6 +44,35 @@ if (isset($_SESSION['admin'])) {
 	</div>
 
 	<?php include 'includes/scripts.php' ?>
+
+	<script>
+		const fondoImagenElement = document.getElementById('fondo');
+		const carpetaFondos = '../img/fondo-admin/';
+		const imagenesFondo = [
+			'Fondo-Panel-de-Control-1.webp',
+			'Fondo-Panel-de-Control-2.webp',
+			'Fondo-Panel-de-Control-3.webp',
+			'Fondo-Panel-de-Control-4_1.webp',
+			'Fondo-Panel-de-Control-5.webp',
+			'Fondo-Panel-de-Control-6.webp',
+		];
+		let indiceAleatorio;
+		let ultimoIndiceAleatorio;
+
+		document.addEventListener("DOMContentLoaded", function() {
+			do {
+				indiceAleatorio = Math.floor(Math.random() * imagenesFondo.length);
+			} while (indiceAleatorio === ultimoIndiceAleatorio);
+
+			ultimoIndiceAleatorio = indiceAleatorio;
+
+			const imagenFondoURL = carpetaFondos + imagenesFondo[indiceAleatorio];
+			fondoImagenElement.style.backgroundImage = `url(${imagenFondoURL})`;
+			fondoImagenElement.style.backgroundRepeat = 'no-repeat';
+			fondoImagenElement.style.backgroundSize = 'cover';
+			fondoImagenElement.style.backgroundAttachment = 'fixed';
+		});
+	</script>
 </body>
 
 </html>
