09c245a4700d1146302dc6dd178ac8a9e4c9462f
EliasVargas18 <eliasaron12.14.04@gmail.com>
2023-07-18 15:03:21 -0500

    modal--

--- admin/includes/employee_modal.php
+++ admin/includes/employee_modal.php
@@ -158,10 +158,6 @@
               <div class="col-sm-4">Entrada</div>
               <div class="col-sm-4">Salida</div>
             </div>
-            
-            <?php 
-            $days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
-            foreach ($days as $index => $day): ?>
               <div class="row" style="margin-bottom: 10px;">
                 <div class="col-sm-3"><?= $day ?></div>
                 <div class="col-sm-4">
@@ -173,47 +169,20 @@
                     while ($srow = $query->fetch_assoc()): ?>
                       <option value="<?= $srow['id'] ?>"><?= $srow['time_in'] ?></option>
                     <?php endwhile; ?>
-                    <option value="Personalizado">Personalizado</option>
                   </select>
                 </div>
                 <div class="col-sm-4">
                   <select class="form-control" name="schedule[<?= strtolower($day) ?>_out]" id="schedule_<?= strtolower($day) ?>_out">
                     <option value="" selected>- Seleccionar -</option>
-
-                    <?php
-                    $query->data_seek(0);
-                    while ($srow = $query->fetch_assoc()): ?>
-                      <option value="<?= $srow['id'] ?>"><?= $srow['time_out'] ?></option>
-                    <?php endwhile; ?>
-
                     <option value="Personalizado">Personalizado</option>
                   </select>
                 </div>
               </div>
-            <?php endforeach; ?>
           </div>
         </div>
-        <script>
-        function updateScheduleOut(selectElement) {
-          var selectedValue = selectElement.value;
-          var selectId = selectElement.id;
-          var day = selectId.split('_')[1];
-
-          var scheduleOutSelect = document.getElementById('schedule_' + day + '_out');
-          var scheduleOutOptions = scheduleOutSelect.options;
-
-          for (var i = 0; i < scheduleOutOptions.length; i++) {
-            var option = scheduleOutOptions[i];
-            if (option.value === selectedValue) {
-              option.selected = true;
-              break;
-            }
-          }
-        }
-        </script>
           	</div>
           	<div class="modal-footer">
-              <a href="secondmodal" data-dismiss="modal">Personalizar</a>
+              <a href="#secondmodal" class="z-0 position-absolute p-5 rounded-3" class="btn btn-primary btn-flat" data-toggle="modal" >Personalizar</a>
             	<button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Cerrar</button>
             	<button type="submit" class="btn btn-primary btn-flat" name="add"><i class="fa fa-save"></i> Guardar</button>
             	</form>
@@ -221,6 +190,92 @@
         </div>
     </div>
 </div>
+
+<div class="modal fade" id="secondmodal" style="position: fixed; top: 80px; left: 150px;">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <h4 class="modal-title text-center"><b>Personalizar horario</b></h4>
+      </div>
+      <div class="modal-body modal-tamanio">
+        <form class="form-horizontal" method="POST" action="employee_add.php" enctype="multipart/form-data">
+          <div class="form-group">
+            <label for="schedule" class="col-sm-3 control-label">Horario semanal</label>
+            <div class="col-sm-9">
+              <div class="row" style="margin-bottom: 10px;">
+                <div class="col-sm-3">&nbsp;</div>
+                <div class="col-sm-4">Entrada</div>
+                <div class="col-sm-4">Salida</div>
+              </div>
+              <?php
+              $days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+
+              foreach ($days as $index => $day) {
+                $day_lower = strtolower($day);
+                ?>
+
+                <div class="row" style="margin-bottom: 10px;">
+                  <div class="col-sm-3"><?php echo $day; ?></div>
+                  <div class="col-sm-4">
+                    <select class="form-control" name="schedule[<?php echo $day_lower; ?>_in]" id="schedule_<?php echo $day_lower; ?>_in" onchange="updateScheduleOut(this)">
+                      <option value="" selected>- Seleccionar -</option>
+                      <?php
+                      $sql = "SELECT * FROM schedules";
+                      $query = $conn->query($sql);
+                      while ($srow = $query->fetch_assoc()) {
+                        echo '<option value="' . $srow['id'] . '">' . $srow['time_in'] . '</option>';
+                      }
+                      ?>
+                      <option value="Personalizado">Personalizado</option>
+                    </select>
+                  </div>
+                  <div class="col-sm-4">
+                    <select class="form-control" name="schedule[<?php echo $day_lower; ?>_out]" id="schedule_<?php echo $day_lower; ?>_out">
+                      <option value="" selected>- Seleccionar -</option>
+                      <?php
+                      $query->data_seek(0);
+                      while ($srow = $query->fetch_assoc()) {
+                        echo '<option value="' . $srow['id'] . '">' . $srow['time_out'] . '</option>';
+                      }
+                      ?>
+                      <option value="Personalizado">Personalizado</option>
+                    </select>
+                  </div>
+                </div>
+
+              <?php
+              }
+              ?>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script>
+  
+  function updateScheduleOut(selectElement) {
+    var selectedValue = selectElement.value;
+    var selectId = selectElement.id;
+    var day = selectId.split('_')[1];
+
+    var scheduleOutSelect = document.getElementById('schedule_' + day + '_out');
+    var scheduleOutOptions = scheduleOutSelect.options;
+
+    for (var i = 0; i < scheduleOutOptions.length; i++) {
+      var option = scheduleOutOptions[i];
+      if (option.value === selectedValue) {
+        option.selected = true;
+        break;
+      }
+    }
+  }
+</script>
 
 <!-- Edit -->
 <div class="modal fade" id="edit">
